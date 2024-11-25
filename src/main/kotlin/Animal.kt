d600caa35ca28b0e104d453313f634af2fcaf7f1
Morgan Smith <morgan.smith@holbmac3488.home>
2024-11-25 11:02:50 +0000

    Adding the class 'Animal' and creating object instances of it in Main.kt

--- src/main/kotlin/Animal.kt
+++ src/main/kotlin/Animal.kt
@@ -1,2 +1,13 @@
-class Animal {
+class Animal(name: String = "Animal", sound: String = "Animal sound") {
+    var animalName: String = name
+    var animalSound: String = sound
+
+    // initialising Animal class
+    init {
+        println("Animal created successfully.")
+    }
+
+    fun makeSound() {
+        println("$animalName makes sound: $animalSound")
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    // classes
+    // Classes and Objects
+
+    val animal = Animal("Dog", "Woof Woof!")
+    animal.makeSound()
+
+    // uses default name 'Animal' and sound 'Animal sound'
+    val animal2 = Animal()
+    animal2.makeSound()
 }
